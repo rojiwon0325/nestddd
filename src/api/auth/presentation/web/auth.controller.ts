59e90b729587757417412f1528346f47101ebde0
rojiwon0325 <bond9986@naver.com>
2022-11-10 02:43:54 +0900

    swagger 주석 추가

--- src/api/auth/presentation/web/auth.controller.ts
+++ src/api/auth/presentation/web/auth.controller.ts
@@ -12,12 +12,21 @@ export class AuthController {
     private readonly usecase: IAuthUsecase,
   ) {}
 
+  /**
+   * 사용자 로그인 API
+   * @tags auth
+   * @throw 404 존재하지 않는 사용자인 경우
+   * @throw 400 비밀번호가 일치하지 않거나 이메일 인증이 완료되지 않은 경우
+   * @param body 로그인에 사용되는 이메일과 비밀번호를 포함한다
+   * @returns access_token 전달, 해당 토큰은 쿠키에도 저장됨
+   */
   @Post('sign-in')
   @HttpCode(202)
   async signIn(
-    @helper.TypedBody() { email, password }: IAuthUsecase.SignIn,
+    @helper.TypedBody() body: IAuthUsecase.SignIn,
     @Res({ passthrough: true }) res: Response,
   ) {
+    const { email, password } = body;
     const [name, token, config] = await this.usecase.signIn({
       email,
       password,
@@ -26,6 +35,11 @@ export class AuthController {
     return { status: 202, access_token: token };
   }
 
+  /**
+   * 로그아웃 API
+   * 사용자 쿠키에 있는 토큰을 제거한다.
+   * 추후에 별도의 인증서버를 연동할 경우, 해당 인증서버에 로그아웃 요청을 보낸다.
+   */
   @Get('sign-out')
   signOut(@Res({ passthrough: true }) res: Response) {
     res.clearCookie(Cookie.name);
